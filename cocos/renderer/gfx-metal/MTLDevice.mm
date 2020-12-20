073f9caa7c4b770b89f271eb6a1cdb9c32004581
pandamicro <lphuabin@gmail.com>
2020-12-20 14:36:46 +0800

    Add usage for dss texture

--- cocos/renderer/gfx-metal/MTLDevice.mm
+++ cocos/renderer/gfx-metal/MTLDevice.mm
@@ -86,6 +86,7 @@ bool CCMTLDevice::initialize(const DeviceInfo &info) {
     textureDescriptor.width = info.width;
     textureDescriptor.height = info.height;
     textureDescriptor.storageMode = MTLStorageModePrivate;
+    textureDescriptor.usage = MTLTextureUsageRenderTarget;
     _dssTex = [mtlDevice newTextureWithDescriptor:textureDescriptor];
     _stencilBits = 8;
 
