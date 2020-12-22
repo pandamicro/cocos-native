efea1cf2fc0806e6c1102d10a9a16e7a199d2e1e
pandamicro <lphuabin@gmail.com>
2020-12-22 23:48:47 +0800

    Autorelease pool using thread_local pools

--- cocos/renderer/gfx-metal/MTLDevice.mm
+++ cocos/renderer/gfx-metal/MTLDevice.mm
@@ -29,6 +29,8 @@
 namespace cc {
 namespace gfx {
 
+thread_local NSAutoreleasePool *autoReleasePool = nullptr;
+
 CCMTLDevice::CCMTLDevice() {
     _clipSpaceMinZ = 0.0f;
     _screenSpaceSignY = 1.0f;
@@ -187,6 +189,7 @@ void CCMTLDevice::resize(uint width, uint height) {}
 void CCMTLDevice::acquire() {
     _inFlightSemaphore->wait();
 
+    autoReleasePool = [[NSAutoreleasePool alloc] init];
     // Clear queue stats
     CCMTLQueue *queue = static_cast<CCMTLQueue *>(_queue);
     queue->_numDrawCalls = 0;
@@ -213,6 +216,8 @@ void CCMTLDevice::present() {
         _inFlightSemaphore->signal();
     }];
     [mtlCommandBuffer commit];
+    [autoReleasePool release];
+    autoReleasePool = nullptr;
 }
 
 Fence *CCMTLDevice::createFence() {
